d497cea95f45ef6ee806b419769fb7e578859a5e
saqib <saqib@saqibs-MacBook-Air.local>
2025-09-16 01:53:17 +0530

    files

---
+++ constants/index.ts
@@ -0,0 +1,94 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_travelgraph_work', label: 'How Travelgraph Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Offline Maps for Any Adventure',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'TravelGraph lets you explore hiking and trekking trails even without an internet connection. Always stay on track with offline maps available anytime, anywhere.',
+  },
+  {
+    title: 'Plan Your Adventure Easily',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      'Schedule your trips, hikes, and trekking adventures with friends. TravelGraph helps you organize your plans and grab the best offers on holidays, tickets, and travel passes.',
+  },
+  {
+    title: 'Augmented Reality Guides',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Navigate trails, forests, and mountains using AR technology. TravelGraph provides real-time guidance even offline, making your journeys safer and more fun.',
+  },
+  {
+    title: 'Discover New Destinations Monthly',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Explore fresh hiking trails, trekking spots, and scenic travel destinations every month. Join a global community of adventurers sharing their best experiences.',
+  },
+  {
+    title: 'Book Tickets Seamlessly',
+    icon: '/map.svg',
+    variant: 'blue',
+    description:
+      'TravelGraph makes it easy to book train, bus, and adventure tickets directly in the app, ensuring smooth travel from start to finish.',
+  },
+];
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About Hilink',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '7006123456' },
+    { label: 'Email Officer', value: 'travelGraph@gmail.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
